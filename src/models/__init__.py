87b2673e30068749bf547a98957c73b21d2b54d1
Vittor Lima <vittorlima@discente.ufg.br>
2026-04-24 20:10:50 +0000

    atualiza módulo de transcrição de áudio com importações de modelos específicos

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,8 @@
-""" Módulo para modelos de transcrição de áudio. """
+"""Módulo para modelos de transcrição de áudio."""
+
+# Importações de modelos específicos
+from .deepgram import Deepgram
+from .speechmatics import Speechmatics
+from .parakeet import Parakeet
+from .fasterwhisper import FasterWhisper
+from .elevenlabs import ElevenLabs
